d86bc5a214710aed581f694bf4030852a644bcd8
sbarnett <sfhbarnett91@gmail.com>
2022-06-18 18:03:45 +0200

    Altered video to colour-code based on speed

--- main_monolayer.m
+++ main_monolayer.m
@@ -96,20 +96,52 @@ if plotting
 end
 
 %%
-tj = trajectories(vectorfield);
-tj(tj==0) = NaN;
+%tj = trajectories(vectorfield);
+%tj(tj==0) = NaN;
 % Give the video a name! 
-myVideo = VideoWriter('monolayer.avi');
+myVideo = VideoWriter('monolayer_smooth_color.avi');
 myVideo.FrameRate = 10;
 open(myVideo);
 figure('Position',[200,200,800,800])
 
+maxspeed = 0.2;
+cmap = jet(36);
 for i = 1:126-10
     if i < 10
-        plot(tj(1:i,1:2:end),tj(1:i,2:2:end))
+        distx = diff(tj(1:i,1:2:end)).^2;
+        distx(isnan(distx)) = 0;
+        disty = diff(tj(1:i,2:2:end)).^2;
+        disty(isnan(disty)) = 0;
+        col = sum(distx+disty,1)./i;
+        colpos = round(col./maxspeed.*36)+1;
+        colpos(colpos>256) = 36;
+        for j = 1:36
+            idx = find(colpos==j);
+            toplotx = tj(:,idx.*2-1);
+            toploty = tj(:,idx.*2);
+            plot(toplotx(1:i,:),toploty(1:i,:),'Color',cmap(j,:))
+            hold on
+        end
+        hold off
+        %plot(tj(1:i,1:2:end),tj(1:i,2:2:end))
         axis([0 63 0 63])
     else
-        plot(tj(i-9:i,1:2:end),tj(i-9:i,2:2:end))
+        distx = diff(tj(i-9:i,1:2:end)).^2;
+        distx(isnan(distx)) = 0;
+        disty = diff(tj(i-9:i,2:2:end)).^2;
+        disty(isnan(disty)) = 0;
+        col = sum(distx+disty,1)./10;
+        colpos = round(col./maxspeed.*36)+1;
+        colpos(colpos>36) = 36;
+        for j = 1:36
+            idx = find(colpos==j);
+            toplotx = tj(:,idx.*2-1);
+            toploty = tj(:,idx.*2);
+            plot(toplotx(i-9:i,:),toploty(i-9:i,:),'Color',cmap(j,:))
+            hold on
+        end
+        hold off
+        %plot(tj(i-9:i,1:2:end),tj(i-9:i,2:2:end))
         axis([0 63 0 63])
     end
     pause(0.02) 
