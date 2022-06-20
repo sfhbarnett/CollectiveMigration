a2a1096cae5690ed069636bfd83588d3ce0934a5
sbarnett <sfhbarnett91@gmail.com>
2022-06-20 10:12:25 +0200

    fixed git syncing issue with correlation.m file

--- Correlation.m
+++ Correlation.m
@@ -1,21 +1,10 @@
-path = '/Users/sbarnett/Documents/PIVData/Adil/100nm/WH Zeiss 100 nm_Results/PIV_roi_velocity_text/';
-files = dir(path);
-names = {};
-for i=1:128
-    test = files(i).name;
-    names{i} = files(i).name;
-end
-filessort = natsort(names).';
+function [correl] = Correlation(vectorfieldstack, startframe, endframe)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
 
-
-%%
-
-startframe = 10;
-endframe = 100;
 u = 1;
-vectorfield = csvread(fullfile(path,filessort{1}));
 
-minsize = sqrt(size(vectorfield,1));
+minsize = sqrt(size(vectorfieldstack,1));
 %check if minsize is odd, if so, subtract 1
 if floor(minsize/2) ~= minsize/2
     minsize = minsize-1;
@@ -27,7 +16,7 @@ CY=zeros(endframe-startframe,radius);
 
 for frame = startframe:endframe
     
-    vectorfield = csvread(fullfile(path,filessort{frame}));
+    vectorfield = vectorfieldstack(:,:,frame);
     
     vectorfield(:,1) = vectorfield(:,1)./vectorfield(1,1);
     vectorfield(:,2) = vectorfield(:,2)./vectorfield(1,2);
@@ -35,6 +24,8 @@ for frame = startframe:endframe
     vvx = reshape(vectorfield(:,3),[sqrt(size(vectorfield,1)),sqrt(size(vectorfield,1))]);
     vvy = reshape(vectorfield(:,4),[sqrt(size(vectorfield,1)),sqrt(size(vectorfield,1))]);
     %make sure image width/height is even number of elements
+    vvx(isnan(vvx)==1) = 0;
+    vvy(isnan(vvy)==1) = 0;
     vvx = vvx(1:minsize,1:minsize);
     vvy = vvy(1:minsize,1:minsize);
     
@@ -50,6 +41,7 @@ end
 
 CX=CX/(minsize^2);
 CY=CY/(minsize^2);
-C=mean(CX+CY);
+correl=mean(CX+CY);
 
+end
 
