797658d44a81d7b3a604a9bb7605f337402ea015
sbarnett <sfhbarnett91@gmail.com>
2022-06-27 13:03:09 +0200

    made the vortex map 3 dimensional

--- Vortexfinder.m
+++ Vortexfinder.m
@@ -2,11 +2,13 @@
 
 radius = 5;
 
+ufield = reshape(vectorfield(:,3,im),[63 63]);
+rfield = zeros([size(ufield),size(vectorfield,3)]);
 for i = 1:size(vectorfield,3)
     im = i
     ufield = reshape(vectorfield(:,3,im),[63 63]);
     vfield = reshape(vectorfield(:,4,im),[63 63]);
-    rfield = zeros(size(ufield));
+
     x = 1:radius*2+1;
     y = 1:radius*2+1;
     [xfield,yfield] = meshgrid(x,y);
@@ -19,15 +21,16 @@ for i = 1:size(vectorfield,3)
             if ufield(x,y) == 0 && vfield(x,y) == 0
                 continue
             else
-                rfield(x,y) = RotationalOrderParameter(crop);
+                rfield(x,y,i) = RotationalOrderParameter(crop);
             end
         end
     end
 
-    imagesc(rfield,[0 1])
+    imagesc(rfield(:,:,i),[0 1])
     hold on
     quiver(vectorfield(:,1,im)./max(vectorfield(1,1,im)),vectorfield(:,2,im)./max(vectorfield(1,1,im)),vectorfield(:,3,im),vectorfield(:,4,im),1.5,'r')
-    %axis([20 50 20 50])
+    axis equal tight
+    axis([20 45 20 45])
     pause(0.4)
     hold off
 end
